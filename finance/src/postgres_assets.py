dbc6750cf5e830a2117dea96d5b430b9fdabaa0b
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-01-17 23:13:58 +0100

    Create assets for all tables

--- finance/src/postgres_assets.py
+++ finance/src/postgres_assets.py
@@ -55,8 +55,92 @@ def balance_sheet_table(engine: sqlalchemy.engine.base.Engine, schema: str):
     return Table("balance_sheet", metadata, schema=schema, autoload_with=engine)
 
 
+@asset(
+    ins={
+        "engine": AssetIn("neon_postgres_engine"),
+        "schema": AssetIn("postgres_schema"),
+    }
+)
+def cashflow_table(engine: sqlalchemy.engine.base.Engine, schema: str):
+    """
+    Create a cashflow table object
+    """
+    logger.info("Creating cashflow table object")
+    metadata = MetaData()
+    return Table("cashflow", metadata, schema=schema, autoload_with=engine)
+
+
+@asset(
+    ins={
+        "engine": AssetIn("neon_postgres_engine"),
+        "schema": AssetIn("postgres_schema"),
+    }
+)
+def income_stmt_table(engine: sqlalchemy.engine.base.Engine, schema: str):
+    """
+    Create a income statement table object
+    """
+    logger.info("Creating income statement table object")
+    metadata = MetaData()
+    return Table("income_stmt", metadata, schema=schema, autoload_with=engine)
+
+
+@asset(
+    ins={
+        "engine": AssetIn("neon_postgres_engine"),
+        "schema": AssetIn("postgres_schema"),
+    }
+)
+def financials_table(engine: sqlalchemy.engine.base.Engine, schema: str):
+    """
+    Create a financials table object
+    """
+    logger.info("Creating financials table object")
+    metadata = MetaData()
+    return Table("financials", metadata, schema=schema, autoload_with=engine)
+
+
+@asset(
+    ins={
+        "engine": AssetIn("neon_postgres_engine"),
+        "schema": AssetIn("postgres_schema"),
+    }
+)
+def tickers_list_table(engine: sqlalchemy.engine.base.Engine, schema: str):
+    """
+    Create a tickers list table object
+    """
+    logger.info("Creating tickers list table object")
+    metadata = MetaData()
+    return Table("tickers_list", metadata, schema=schema, autoload_with=engine)
+
+
+@asset(
+    ins={
+        "engine": AssetIn("neon_postgres_engine"),
+        "schema": AssetIn("postgres_schema"),
+    }
+)
+def valid_tickers_table(engine: sqlalchemy.engine.base.Engine, schema: str):
+    """
+    Create a valid tickers table object
+    """
+    logger.info("Creating valid tickers table object")
+    metadata = MetaData()
+    return Table("valid_tickers", metadata, schema=schema, autoload_with=engine)
+
+
 defs = Definitions(
-    assets=[neon_postgres_engine, postgres_schema, balance_sheet_table],
+    assets=[
+        neon_postgres_engine,
+        postgres_schema,
+        balance_sheet_table,
+        cashflow_table,
+        income_stmt_table,
+        financials_table,
+        tickers_list_table,
+        valid_tickers_table,
+    ],
     resources={
         "io_manager": mem_io_manager,
     },
