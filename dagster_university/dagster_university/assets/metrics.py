8bbd6dd5e6b89570bea0cf5bab5339783b0789d3
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-05-16 00:18:22 +0200

    Add asset function for calculating Manhattan taxi trip statistics

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -1,3 +1,36 @@
 from dagster import asset
 
+import plotly.express as px
+import plotly.io as pio
+import geopandas as gpd
+
+import duckdb
+import os
+
 from . import constants
+
+@asset(
+    deps=["taxi_trips", "taxi_zones"]
+)
+def manhattan_stats():
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+        left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
+
