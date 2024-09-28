90b0304ef2fe6581aa20994d219d532cccfca08a
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-09-28 23:51:55 +0200

    Move defs into init file

--- finance/__init__.py
+++ finance/__init__.py
@@ -2,10 +2,15 @@
 from dagster import Definitions, load_assets_from_modules
 
 from .src import postgres_assets, tickers_assets
+from dagster import AssetIn, Definitions, asset, in_process_executor, mem_io_manager
 
 _postgres_assets = load_assets_from_modules([postgres_assets])
 _tickers_assets = load_assets_from_modules([tickers_assets])
 
 defs = Definitions(
-    assets=[*_postgres_assets, *_tickers_assets]
+    assets=[*_postgres_assets, *_tickers_assets],
+    resources={
+        "io_manager": mem_io_manager,
+    },
+    executor=in_process_executor,
 )
